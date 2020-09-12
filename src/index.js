6641d3c859c413528a9512cf43f8c912a329f642
manaskumar26 <67231881+manaskumar26@users.noreply.github.com>
2020-09-12 22:45:53 +0530

    Firebase Fetch

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as firebase from 'firebase';
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDH3I58LzayZUaQ6_hJBWZsJEWCh0aKT1E",
+  authDomain: "shoppingcartreactjs.firebaseapp.com",
+  databaseURL: "https://shoppingcartreactjs.firebaseio.com",
+  projectId: "shoppingcartreactjs",
+  storageBucket: "shoppingcartreactjs.appspot.com",
+  messagingSenderId: "842927852070",
+  appId: "1:842927852070:web:c346198ad356754fdff4e7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 ReactDOM.render(
   <React.StrictMode>
     <App />
