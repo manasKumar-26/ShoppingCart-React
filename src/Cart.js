bbdccd13bcaaccd5c076eb23c218040c7190a273
manaskumar26 <67231881+manaskumar26@users.noreply.github.com>
2020-09-13 12:54:17 +0530

    Firebase Deployed On the known functions

--- src/Cart.js
+++ src/Cart.js
@@ -1,50 +1,75 @@
 import React from 'react';
 import CartItem from './CartItem';
 import NavBar from './Navbar';
+import * as firebase from 'firebase';
 class Cart extends React.Component{
     constructor(){
         super()
         this.state={
-            product:[
-                {
-                    title:'Iphone 11 Pro',
-                    price:999,
-                    qty:1,
-                    img:'https://cdn.mos.cms.futurecdn.net/7TC7pcZvnFa9xPDgU6V6VQ.jpg',
-                    key:Math.floor(Math.random()*99)
-                },
-                {
-                    title:'Apple Watch Series 5',
-                    price:199,
-                    qty:1,
-                    img:'https://wallpaperaccess.com/full/1568237.jpg',
-                    key:Math.floor(Math.random()*99)
-                },
-                {
-                    title:'Apple Macbook Pro',
-                    price:1580,
-                    qty:1,
-                    img:'https://images-na.ssl-images-amazon.com/images/I/71IQiviMzWL._AC_SL1500_.jpg',
-                    key:Math.floor(Math.random()*99)
-                },
-                {
-                    title:'Sony Play-Station 4',
-                    price:899,
-                    qty:1,
-                    img:'https://cdn.wallpapersafari.com/81/10/oniJ0P.png',
-                    key:Math.floor(Math.random()*99)
-                }
-            ],
-            count:4
+            product:[],
+            loading:true,
+            count:0
         }
+        this.db=firebase.firestore()
     }
+    componentDidMount(){
+        // firebase
+        // .firestore()
+        // .collection('product')
+        // .get()
+        // .then((snapshot)=>{
+        //     let product=snapshot.docs.map((product)=>{
+        //         let item=product.data();
+        //         item.key=product.id;
+        //         return item;
+        //     });
+        //     this.setState({
+        //         product,
+        //         count:product.length,
+        //         loading:false
+        //     })
+        // })
+        
+        firebase
+        .firestore()
+        .collection('product')
+        .onSnapshot((snapshot)=>{
+            let count=0;
+            let product=snapshot.docs.map((product)=>{
+                let item=product.data();
+                count+=item.qty
+                item.key=product.id;
+                return item;
+            });
+            this.setState({
+                product,
+                count,
+                loading:false
+            });
+        })
+    }
+    componentDidUpdate(prevProps,prevState){
+        console.log(prevState);
+        console.log(this.state)
+    }
+
     increaseQuantity=(item)=>{
         const { product }=this.state;
         let index=product.indexOf(item);
-        product[index].qty+=1;
-        this.setState({
-            product:product,
-            count:this.state.count+1
+        // product[index].qty+=1;
+        // this.setState({
+        //     product:product,
+        //     count:this.state.count+1
+        // })
+        let docref=this.db.collection('product').doc(product[index].key);
+        docref.update({
+            qty:product[index].qty+1,
+        })
+        .then(()=>{
+            console.log('Updated')
+        })
+        .catch((err)=>{
+            console.log(err);
         })
     }
     decreaseQuantity=(item)=>{
@@ -53,19 +78,38 @@ class Cart extends React.Component{
         if(product[index].qty<=0){
             return;
         }
-        product[index].qty-=1;
-        this.setState({
-            product,
-            count:this.state.count-1
+        let docref=this.db.collection('product').doc(product[index].key);
+        docref.update({
+            qty:product[index].qty-1,
         })
+        .then(()=>{
+            console.log('Dec');
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+        // product[index].qty-=1;
+        // this.setState({
+        //     product,
+        //     count:this.state.count-1
+        // })
     }
     deleteItem=async (id)=>{
-        const { product }=this.state;
-        let item=await product.filter((item)=>item.key !== id);
-        let deleteItem=await product.find((item)=> item.key === id);
-        this.setState({
-            product:item,
-            count:this.state.count-deleteItem.qty
+        // const { product }=this.state;
+        // let item=await product.filter((item)=>item.key !== id);
+        // let deleteItem=await product.find((item)=> item.key === id);
+        // this.setState({
+        //     product:item,
+        //     count:this.state.count-deleteItem.qty
+        // })
+        let docref=this.db.collection('product').doc(id);
+        docref
+        .delete()
+        .then(()=>{
+            console.log('Deleted');
+        })
+        .catch((err)=>{
+            console.log(err);
         })
     }
     totalAmount=()=>{
@@ -76,11 +120,25 @@ class Cart extends React.Component{
         });
         return totalAmount;
     }
+    // addProduct=()=>{
+    //     this.db
+    //     .collection('product')
+    //     .add({
+    //         title:'Series',
+    //         qty:1,
+    //         price:'456',
+    //         img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_u5IjVt8L0lLW9Ser46YhPjES0usGEeZHQQ&usqp=CAU',
+    //     })
+    //     .then((newDoc)=>{
+    //         console.log(newDoc)
+    //     })
+    // }
     render(){
-        const { product }=this.state
+        const { product,loading }=this.state
         return(
             <React.Fragment>
             <NavBar count={this.state.count}/>
+            {/* <button onClick={this.addProduct} style={style.button}>Add A Product</button> */}
             <div className="cartBox">
                 {product.map((item)=>{
                     return <CartItem 
@@ -92,10 +150,17 @@ class Cart extends React.Component{
                     />
                 })}
             </div>
+            {loading && <div className="loading"><img src="https://faviconer.net/img/ajax-loader-preview.png" alt="login"/><h1>Fetching Your Cart Items....</h1></div>}
             <hr/>
             <div className="checkout">Total Payable Amount : {this.totalAmount()}</div>
             </React.Fragment>
         );
     }
 }
+// const style={
+//     button:{
+//         padding:20,
+//         background:'transparent',
+//     }
+// }
 export default Cart;
